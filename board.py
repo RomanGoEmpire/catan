48eee95f3254f0e19327eeddda9fc580134dd98e
Roman Gerloff <71276897+RomanGoEmpire@users.noreply.github.com>
2023-09-24 20:54:15 +0200

    Update board.py

---
+++ board.py
@@ -0,0 +1,9 @@
+class Board:
+    def __init__(self, players):
+        self.players = players
+        self.tile_nodes = None
+        self.building_nodes = None
+        self.generate_board()
+
+    def generate_board(self):
+        pass
